8216489efe99b13ac06efb33fecad7d7e2159054
Callum Anderson <crea1g15@soton.ac.uk>
2020-07-14 22:50:25 +0100

    started networking

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Message
+{
+    private DateTime hitTime;
+    private int userID;
+
+    public Message(int userID)
+    {
+        this.userID = userID;
+        hitTime = DateTime.Now;
+    }
+
+    public DateTime GetTime()
+    {
+        return hitTime;
+    }
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Server : MonoBehaviour
+{
+
+    private Socket listener;
+    private Thread listen;
+
+    private void Awake()
+    {
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr = ipHost.AddressList.Where(x => x.AddressFamily == AddressFamily.InterNetwork).First();
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 12345);
+
+        Debug.Log(localEndPoint.ToString());
+
+        listener = new Socket(AddressFamily.InterNetwork, SocketType.Rdm, ProtocolType.Tcp);
+        try
+        {
+            listener.Bind(localEndPoint);
+
+            ThreadStart listenStart = new ThreadStart(Listener);
+            listen = new Thread(listenStart);
+            listen.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);          
+        }
+    }
+
+    void Listener()
+    {
+        listener.Listen(10);
+
+        while (true)
+        {
+            Debug.Log("Waiting connection ... ");
+
+            Socket clientSocket = listener.Accept();
+
+            // Data buffer 
+            List<ArraySegment<byte>> buffer = new List<ArraySegment<byte>>();
+
+            int numByte = clientSocket.Receive(buffer);
+
+            byte[] x = new byte[0];
+            foreach(ArraySegment<byte> seg in buffer){
+                x = x.Concat(seg).ToArray();
+            }
+
+            Message msg = ByteArrayToMessage(x);
+
+            Debug.Log(msg.ToString());
+
+            // Close client Socket using the 
+            // Close() method. After closing, 
+            // we can use the closed Socket  
+            // for a new Client Connection 
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+
+    public static Message ByteArrayToMessage(byte[] arrBytes)
+    {
+        using (var memStream = new MemoryStream())
+        {
+            var binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            var obj = binForm.Deserialize(memStream);
+            return (Message) obj;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
